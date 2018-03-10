63b992ddad034148acfcd9b2bebd229f9d597f3f
Stuart Mathews <stumathews@gmail.com>
2018-03-10 11:52:48 +0000

    Update deploy-envs.sh
    new external ip address

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=35.177.172.56
+export AWS_VIRTUAL_HOST=35.178.127.161
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
