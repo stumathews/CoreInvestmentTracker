766e8603600f02c01cdd49a8df480d85a5380f2a
Stuart Mathews <stumathews@gmail.com>
2018-03-11 17:49:06 +0000

    Update deploy-envs.sh

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -5,7 +5,7 @@ export IMAGE_NAME=coreinvestmenttracker
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=eu-west-2
-export AWS_ECS_CLUSTER_NAME=default
+export AWS_ECS_CLUSTER_NAME=core-investment-tracker-api-cluster
 export AWS_VIRTUAL_HOST=35.178.127.161
 
 # set any sensitive information in travis-ci encrypted project settings:
