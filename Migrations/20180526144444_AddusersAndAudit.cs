c66a8f01160df6796786202676023e5376329991
Stuart Mathews <stuart.mathews@finbourne.com>
2018-05-26 15:46:37 +0100

    Add UsersAndAduting db migration file

---
+++ Migrations/20180526144444_AddusersAndAudit.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoreInvestmentTracker.Migrations
+{
+    public partial class AddusersAndAudit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    DisplayName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    TimeZone = table.Column<int>(nullable: false),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecordedActivities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OwningEntityId = table.Column<int>(nullable: false),
+                    OwningEntityType = table.Column<int>(nullable: false),
+                    AtTime = table.Column<DateTimeOffset>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Details = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Tag = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PrimaryKey_Id_OwningEntityId_OwningEntityType", x => new { x.Id, x.OwningEntityId, x.OwningEntityType });
+                    table.ForeignKey(
+                        name: "FK_RecordedActivities_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecordedActivities_UserId",
+                table: "RecordedActivities",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecordedActivities");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
