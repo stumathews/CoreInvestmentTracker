6912c94036fc843e63117dc1b70ced2e12b3cdf9
Stuart Mathews <stumathews@gmail.com>
2019-02-14 14:06:21 +0000

    Add api call to get customentity types associated with owning entity (ByOwningId)

--- Controllers/EntityTypeController.cs
+++ Controllers/EntityTypeController.cs
@@ -6,7 +6,9 @@ using CoreInvestmentTracker.Common;
 using CoreInvestmentTracker.Common.ActionFilters;
 using CoreInvestmentTracker.Models.DAL.Interfaces;
 using CoreInvestmentTracker.Models.DEL;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreInvestmentTracker.Controllers
 {
@@ -20,6 +22,19 @@ namespace CoreInvestmentTracker.Controllers
         /// <param name="entityRepository"></param>
         public CustomEntityTypeController(IEntityApplicationDbContext<CustomEntityType> entityRepository) : base(entityRepository)
         {
+        }
+
+        /// <summary>
+        /// Gets the custom entity types that have entities of the type for this owning entity
+        /// </summary>
+        /// <param name="owningId">owning entity</param>
+        /// <returns>Custom Types associated with this entity (ie that have custom entities associated with this entity)</returns>
+        [HttpGet("ByOwningId/{owningId}"), Authorize]
+        public IEnumerable<CustomEntityType> ByOwningId( int owningId)
+        {
+            var ids = EntityRepository.Db.CustomEntities.Where(x => x.OwningEntityId == owningId).Select(y=>y.CustomEntityType.Id).Distinct();
+            var types = EntityRepository.Db.CustomEntityTypes.Where(x => ids.Contains(x.Id));
+            return types;
         }
     }
 }
